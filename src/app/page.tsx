84eb794a8e52e4dba37c38591e81b107451ca6ed
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-12-13 20:07:14 -0300

    Feat: alteração de cor dinâmica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import { Menu } from "@/components/Menu";
 import { useState } from "react";
 
 export default function Home() {
-  const [showHighlight, setShowHighlight] = useState<boolean>(true);
+  const [showHighlight, setShowHighlight] = useState<boolean>(false);
+  const [codeEditorBgColor, setCodeEditorBgColor] = useState<string>("#6BD1FF");
 
   return (
     <main className="grid grid-cols-4 lg:mt-8 h-[calc(100vh-152px)] mt-4 overflow-y-auto">
@@ -14,7 +15,7 @@ export default function Home() {
         <Menu />
       </aside>
       <section className="col-span-4 lg:col-span-2 p-4">
-        <CodeEditor showHighlight={showHighlight} />
+        <CodeEditor bgColor={codeEditorBgColor} showHighlight={showHighlight} />
         <button
           onClick={() => setShowHighlight(!showHighlight)}
           className="bg-[#5081FB14] h-14 rounded-lg text-white w-full mt-8"
@@ -23,7 +24,9 @@ export default function Home() {
         </button>
       </section>
       <aside className="col-span-4 lg:col-span-1 lg:flex">
-        <InsertForm />
+        <InsertForm
+          handleColor={(e: any) => setCodeEditorBgColor(e.target.value)}
+        />
       </aside>
     </main>
   );
