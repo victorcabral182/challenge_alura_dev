9370fd8a20d041b08e9a8c004a5a5eeca9747d90
Victor Cabral <victor.cabral@globalsys.com.br>
2023-12-19 14:20:53 -0300

    Refactor: Componentizing and Adjusting Details

--- src/components/InsertForm/index.tsx
+++ src/components/InsertForm/index.tsx
@@ -1,5 +1,10 @@
+import { Button } from "../Button"
+import { InputSelect } from "../InputSelect"
+import { InputText } from "../InputText"
+import { InputTextArea } from "../InputTextArea"
+
 interface InsertFormProps {
-  handleColor: (e: any) => void;
+  handleColor: (e: any) => void
 }
 
 export const InsertForm = ({ handleColor }: InsertFormProps) => {
@@ -8,56 +13,36 @@ export const InsertForm = ({ handleColor }: InsertFormProps) => {
       label: "JavaScript",
       value: "javascript",
     },
-    {
-      label: "TypeScript",
-      value: "typescript",
-    },
-    {
-      label: "Java",
-      value: "java",
-    },
-    {
-      label: "C#",
-      value: "csharp",
-    },
-  ];
+  ]
   return (
     <>
-      <section className="flex flex-col gap-7 p-5 lg:pr-0">
+      <section className="w-full flex flex-col gap-7 p-5 lg:pr-0">
         <p className="text-white text-[14px] uppercase">Seu projeto</p>
-        <input
-          type="text"
-          className=" w-full h-14 p-4 rounded-lg bg-[#ffffff] bg-opacity-[16%] text-white focus-within:outline-none lg:col-span-2"
-          placeholder="Nome do seu projeto"
+        <InputText
+          id="name"
+          label="Nome"
+          placeholder="Digite o nome do projeto"
         />
-        <textarea
-          className=" w-full h-[120px] p-4 rounded-lg bg-[#ffffff] bg-opacity-[16%] text-white focus-within:outline-none lg:col-span-2"
-          placeholder="Descrição do seu projeto"
+        <InputTextArea
+          id="description"
+          label="Descrição"
+          placeholder="Descreva o seu projeto"
         />
         <p className="text-white text-[14px] uppercase">Personalização</p>
         <div className="flex flex-col md:flex-row lg:flex-col gap-6">
-          <select
-            className=" w-full h-14 p-4 rounded-lg bg-[#ffffff] bg-opacity-[16%] text-white focus-within:outline-none lg:col-span-2"
-            placeholder="Nome do seu projeto"
-          >
-            {languages.map((item) => (
-              <option className="text-black" key={item.value}>
-                {item.label}
-              </option>
-            ))}
-          </select>
+          <InputSelect label="Linguagem" options={languages} />
           <input
             defaultValue="#5081FB"
             onChange={handleColor}
             type="color"
-            className=" w-full h-14 p-1 rounded-lg bg-[#ffffff] bg-opacity-[16%] text-white focus-within:outline-none lg:col-span-2"
+            className="w-full h-14 p-1 rounded-lg bg-[#ffffff] bg-opacity-[16%] text-white focus-within:outline-none lg:col-span-2"
             placeholder="Descrição do seu projeto"
           />
         </div>
-        <button className="bg-[#5081FB] h-14 rounded-lg w-full" type="button">
+        <Button variant="filled" type="button">
           Salvar projeto
-        </button>
+        </Button>
       </section>
     </>
-  );
-};
+  )
+}
